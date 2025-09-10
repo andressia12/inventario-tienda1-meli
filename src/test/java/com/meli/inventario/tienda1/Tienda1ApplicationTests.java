efad73d5adbd4001f509e98c68618538cdeb6d9a
Andrés Felipe Siachoque Alarcón <andressiachoque96@hotmail.com>
2025-09-09 22:54:29 -0500

    Primer commit - Proyecto inicial

---
+++ src/test/java/com/meli/inventario/tienda1/Tienda1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.inventario.tienda1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tienda1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
